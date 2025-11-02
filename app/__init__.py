e5dda6c69ea5225d782eba8f3e9e548f5cfa7a3f
Miguel Gomez <miguel.gomezalarcon@mercadolibre.com.co>
2025-11-02 18:09:57 -0500

    fix ping endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -22,10 +22,12 @@ def create_app():
     from app.routes.blacklists import blacklists_bp
     from app.routes.blacklists_get import blacklists_get_bp
     from app.routes.health import health_bp
+    from app.routes.ping import ping_bp
 
     app.register_blueprint(blacklists_bp, url_prefix='/blacklists')
     app.register_blueprint(blacklists_get_bp, url_prefix='/blacklists')
     app.register_blueprint(health_bp)
+    app.register_blueprint(ping_bp)
 
     # Create tables
     with app.app_context():
