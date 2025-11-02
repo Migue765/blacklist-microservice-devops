90b470983dc2bbb85fe0c7d93d7a44177b20ce42
Miguel Gomez <miguel.gomezalarcon@mercadolibre.com.co>
2025-11-02 18:03:11 -0500

    update ping endpint

---
+++ app/routes/ping.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, jsonify
+from app.utils import setup_logging
+
+ping_bp = Blueprint('ping', __name__)
+logger = setup_logging()
+
+@ping_bp.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"status": "ok"}), 200
